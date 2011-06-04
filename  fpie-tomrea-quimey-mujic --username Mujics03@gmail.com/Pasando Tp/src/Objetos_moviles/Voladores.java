dda757b944bc216b1bf8e078aa5f9e39c16c1d81
Mujics03@gmail.com <Mujics03@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-04 14:10:49 +0000

    Clase Voladores

---
+++  fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/Voladores.java
@@ -0,0 +1,77 @@
+package Objetos_moviles;
+import java.util.LinkedList;
+import Escenario.*;
+import Excepciones.ChauBlindajeException; 
+import Armas.*;
+import Objetos_moviles.*;
+
+public class Voladores extends ObjetosMoviles{
+
+	boolean vivo;
+	int Timing;
+	ControladorArmas weapons;
+	
+	protected void ActuarAnteColision(ObjetosMoviles movil){
+		// Los aviones se dañan entre si
+		Danio danioTemp = this.fuerzaDeChoque(); 
+		this.analizarDanio( movil.fuerzaDeChoque() );
+		movil.analizarDanio( danioTemp );
+	}
+	
+	
+	public boolean PuedeManejarItems(){
+		return true;
+	}
+	
+	public boolean PuedeSerAtacado(){
+		return true;
+	}
+	
+	
+	protected void VerificarColicion(){
+		Escenario escenario = escenario.getInstance();
+		// buscar tipo generico Collection
+		Collection todoLoQueEstaEnJuego = escenario.objetosVivos();
+		int j; 
+		for ( j = 0 , j = todoLoQueEstaEnJuego.size() , j++ ){
+			ObjetosMoviles elemento = todoLoQueEstaEnJuego.get( j );
+			if ( this.condicionComun(elemento) ) elemento.ActuarAnteColision(this);
+			}
+		}
+	
+	public void agregarArma( ArmasFisicas arma ){
+		if ( this.EstaVivo() ) weapons.add(arma) {
+		} else throw new ChauBlindajeException();
+	}
+		
+	protected Municion disparar(){
+		// Corregir metodo weapons
+		if ( this.EstaVivo() ) weapons.dispararElArmaSeleccionadaDesde( ubicacion XY ){
+		} else throw new ChauBlindajeException();
+	}
+	
+	protected Danio fuerzaDeChoque(){
+		// verificar porque no reconoce el new, d dond sale blindaje
+		Danio danio =  new Danio();
+		danio.Danio( blindaje.getHitPoints() );
+		return danio;
+	}
+	
+	public void main(String[] args) {
+		vivo = true;
+		Timing = 0;
+		weapons = new ControladorArmas();
+	}
+
+	protected void manejarItemVida( ItemVida item ){
+		// de dond sale blindaje
+		if ( this.EstaVivo() ) blindaje.repararPor( item ) {
+		} else throw new ChauBlindajeException();
+	}
+	
+	protected void seleccionarSiguienteArma(){
+		// no reconoce weapons
+		if ( this.EstaVivo() ) weapons.seleccionarSiguienteArma(){
+		} else throw new ChauBlindajeException();
+	}
+}
