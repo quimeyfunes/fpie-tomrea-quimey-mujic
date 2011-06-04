513f937121e741c615660835e95aa7bf216e7b06
fpiechotka@gmail.com <fpiechotka@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-04 05:00:05 +0000

    Agrego carpeta borrada por error

---
+++  fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/EstrategiaDeVuelo.java
@@ -0,0 +1,25 @@
+package Objetos_moviles;
+
+import Mapa.Ubicacion;
+
+public abstract class EstrategiaDeVuelo{
+	
+	//movimiento lateral igual a 1/10 del tamanio del escenario
+	private static double slide= (Ubicacion.getLimiteX()/10);
+	private static double MinimoMovimiento = 1;
+	
+	public double Parametro=0;
+	
+	public static double getMinimoMovimiento(){
+		return MinimoMovimiento;
+	}
+	public static double getSlide(){
+		return slide;
+	}
+	public void restartearParametro(){
+		this.Parametro=0;
+	}
+	
+	public abstract void CalcularMovimiento(ObjetosMoviles objeto);
+
+}
---
+++  fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/Items.java
@@ -0,0 +1,23 @@
+package Objetos_moviles;
+
+public class Items extends Objetos_moviles{
+	
+	public void Verificar_colicion(){
+		//borre lo que estaba aca
+	}
+
+	
+	public boolean Maneja_items(){
+		return false;
+	}
+	
+	public boolean Puede_ser_atacado(){
+		return false;
+	}
+	
+	public void Direccion(){
+	}
+	
+	public void Mover_segun_velocidad(){
+	}
+}
