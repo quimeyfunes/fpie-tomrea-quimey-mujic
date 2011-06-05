a1ee12d9ae0acf208c74ab55a643f17263e487b5
Mujics03@gmail.com <Mujics03@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-05 14:18:01 +0000

    Clase item e hijos moficada..falta interfaz para meotodos

---  fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/ItemVida.java
+++  fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/ItemVida.java
@@ -1,6 +1,10 @@
 package Objetos_moviles;
 
-public class ItemVida
+import java.awt.geom.Rectangle2D;
+
+import Mapa.Vector2D;
+
+public class ItemVida extends Items
 {
 	short hitPointsAsumar;
 	
@@ -9,4 +13,17 @@ public class ItemVida
 		return hitPointsAsumar;		
 	}
 	
-}
+	public void ActuarAnteColision(ObjetosMoviles objeto){
+		//no reconoce el metodo porq esta en voladores
+		objeto.manejarItemVida( this );
+		this.Destructor();
+		
+	}
+	
+	public void EstadoCorrecto() {
+		Vector2D p = ubicacion.XY();
+		Cuerpo = new Rectangle2D.Double( p.x,p.y,1,1);
+	}
+	
+	// falta el initialize..lo pongo en main??
+}	
