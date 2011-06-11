fbc4c85310071116efe4f9b866474644379a5c85
tomasreale@gmail.com <tomasreale@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-11 21:34:29 +0000

    Movimiento circular funcionando OK.

--- trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/Circular.java
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Objetos_moviles/Circular.java
@@ -1,6 +1,9 @@
 package Objetos_moviles;
 
 import Mapa.Vector2D;
+
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.lang.Math;
 
 
@@ -23,15 +26,15 @@ public class Circular extends EstrategiaDeVuelo {
 		SafarDeLimiteIzquierdo = (this.VectorQueGira.y>0)&& (this.VectorQueGira.x>0);
 		SafarDeLimiteDerecho = (this.VectorQueGira.y<0) && (this.VectorQueGira.x<0);
 		
-		if ( (LimR) && (SafarDeLimiteIzquierdo==false) ){
+		if ( (LimR) && (!SafarDeLimiteIzquierdo) ){
 			objeto.abajo();
 		}
 		
-		if ( (LimL) && (SafarDeLimiteDerecho==false)){
+		if ( (LimL) && (!SafarDeLimiteDerecho)){
 			objeto.arriba();
 		}
 		
-		if ( (LimR=false) && (LimL=false)){
+		if ( (!LimR) && (!LimL)){
 			objeto.direccion(this.VectorQueGira);
 		}
 		
@@ -43,21 +46,16 @@ public class Circular extends EstrategiaDeVuelo {
 		
 		//http://www.velocityreviews.com/forums/t150749-rotate-around-non-origin-pivot-point.html
 			
-			double xtemp;
-			double a= this.MinimoAnguloCircular;
-			double x= this.VectorQueGira.x;
-			double y= this.VectorQueGira.y;
+		Point2D original = this.VectorQueGira;
+		AffineTransform at = AffineTransform.getRotateInstance(this.MinimoAnguloCircular, 0, 0);
+		Point2D rotated = at.transform(original, null);
 			
-			xtemp = (x * (float)Math.cos(a)) - (y * (float)Math.sin(a));
-			y = (x * (float)Math.sin(a)) + (y * (float)Math.cos(a));
-			x = xtemp;
-			
-			this.VectorQueGira.setLocation(x, y);
+			this.VectorQueGira.setLocation(rotated.getX(), rotated.getY());
 		
 	}
 
 	public static double radioStandarSegunDistancia(Vector2D posicion) {
-		return ((( posicion.y)/2.0)-50);
+		return ((( posicion.y)/2.0)-5);//numero magico para evitar borde
 	}
 	
 	public static double radioStandarSegunDistancia( double x,double y){
@@ -65,8 +63,22 @@ public class Circular extends EstrategiaDeVuelo {
 	}
 
 	public Circular(double radio){
-		this.MinimoAnguloCircular = Math.atan(EstrategiaDeVuelo.getMinimoMovimiento()/radio);
+		this.MinimoAnguloCircular = (Math.atan(EstrategiaDeVuelo.getMinimoMovimiento()/radio));
+		this.SentidoAgujasDelReloj();//default
 		this.VectorQueGira = new Vector2D(1,0);
+	}
+	
+	public void SentidoAgujasDelReloj(){
+		
+		if (this.MinimoAnguloCircular>0){
+			this.MinimoAnguloCircular=this.MinimoAnguloCircular*-1;
+		}
+	}
+	public void SentidoContraAgujasDelReloj(){
+		if (this.MinimoAnguloCircular<0){
+			this.MinimoAnguloCircular=this.MinimoAnguloCircular*-1;
+		}
+		
 	}
 	
 }
--- trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/TestMovimientoCircular.java
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/TestMovimientoCircular.java
@@ -14,35 +14,65 @@ public class TestMovimientoCircular extends TestCase {
 	private Exploradores Avion;
 	private Vector2D puntoCentralPrueba;
 	private double radio;
+	private double posicionX;
+	private double posicionY;
 	
 	@Before
 	public void setUp() throws Exception {
 		Escenario.reiniciarEscenario();
 		
-		Avion = new Exploradores((double)800,(double)800);
+		 posicionX=800;
+		 posicionY=800;
+		
+		Avion = new Exploradores((double)posicionX,(double)posicionY);
 
-		puntoCentralPrueba  = new Vector2D(800,( 800 - (Circular.radioStandarSegunDistancia((double)800,(double)800) )  ));
+		puntoCentralPrueba  = new Vector2D(posicionX,( posicionY - (Circular.radioStandarSegunDistancia((double)posicionX,(double)posicionY) )));
 
-		radio = Circular.radioStandarSegunDistancia((double)800,(double)800);
+		radio = Circular.radioStandarSegunDistancia((double)posicionX,(double)posicionY);
 		
 	}
 	
+	private boolean ComprobarDistancia(){
+		
+		double distancia = this.puntoCentralPrueba.distance(Avion.getPosicion() );
+		Boolean booleano = ( distancia >= (radio - 1) ) && (distancia < ( radio + 1 ) );
+		//debido a redondeo debo tener en cuenta incerteza
+		return booleano;
+	}
+	
 	public void testMovimiento() {
-		for( int i = 0 ; i < 2345; i++ ){
+		
+		double PerimetroCirculo = 2*(Math.PI)* (this.radio);
+		double turnos = PerimetroCirculo/3/4;
+		//consigo el perimetro del circulo, divido por la cantidad de movimientos por turno del avion
+		//luego, divido por 4, ya que voy a verificar que llegue a 1/4 del recorrido
+		//O sea, el extremo que esta mas a la derecha del circulo, respecto a su centro
+		
+		for( int i = 0 ; i < turnos; i++ ){
 			Avion.moverseIAsegunVel();
 		}
-		double distancia = puntoCentralPrueba.distance(Avion.getPosicion() );
-		Boolean booleano = ( distancia >= (radio - 1) ) || (distancia < ( radio + 1 ) );//debido a redondeo debo tener en cuenta incerteza
-		Assert.assertTrue( booleano );
+		Assert.assertTrue( 	this.ComprobarDistancia() );
 		
-		
-		for( int i = 0 ; i < 1456; i++ ){
+		Assert.assertTrue( (1>(Avion.getPosicion().distance( (posicionX+radio),(posicionY-radio) )) ) );
+	
+		//el resto son comprobacion para ver que no se salga de la distancia del radio
+		for( int i = 0 ; i < 2414; i++ ){
 			Avion.moverseIAsegunVel();
 		}
-
-		distancia = puntoCentralPrueba.distance(Avion.getPosicion() );
-		booleano = ( distancia >= (radio - 1) ) || (distancia < ( radio + 1 ) ); 
-		Assert.assertTrue( booleano );
+ 
+		Assert.assertTrue( 	this.ComprobarDistancia() );
+		
+		for( int i=0;i<7345;i++){
+			Avion.moverseIAsegunVel();			
+		}
+		
+		Assert.assertTrue( 	this.ComprobarDistancia() );
+		
+		for( int i=0;i<2599;i++){
+			Avion.moverseIAsegunVel();			
+		}
+		
+		Assert.assertTrue( 	this.ComprobarDistancia() );
 		
 
 	
