e1d32c66a0e519d61bc7329ca9981cc7a9f74718
fpiechotka@gmail.com <fpiechotka@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-13 06:54:06 +0000

    Vista guia.

---
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Vistas/VistaGuia.java
@@ -0,0 +1,11 @@
+package Vistas;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaGuia extends Imagen
+{
+
+	public VistaGuia() {
+		setNombreArchivoImagen("Guia.jpg");
+	}
+}
