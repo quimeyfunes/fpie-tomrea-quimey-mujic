6dd07c0a70f6fe5effa896083fd7049a7429d0e7
fpiechotka@gmail.com <fpiechotka@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-14 04:59:17 +0000

    Game over

---
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Vistas/VistaGameOver.java
@@ -0,0 +1,14 @@
+package Vistas;
+
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
+import ar.uba.fi.algo3.titiritero.vista.Figura;
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaGameOver extends Imagen
+{
+	public VistaGameOver()
+	{
+		setNombreArchivoImagen("Game over.jpg");
+	}
+
+}
