7cdab71dcb3b6d7e2acef24bcdf525efe74c8ecf
fpiechotka@gmail.com <fpiechotka@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-15 03:32:08 +0000

    Parser andando para los 4 campos.

--- trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/XmlTest.java
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/XmlTest.java
@@ -2,11 +2,6 @@ package Pruebas;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Node;
-import org.dom4j.io.SAXReader;
 import ManejoXml.ParserNivelesXml;
 
 public class XmlTest
@@ -19,7 +14,22 @@ public class XmlTest
 	public void testBlindaje()
 	{
 		parser = new ParserNivelesXml();
-		Assert.assertTrue(parser.getFile().exists());
-		parser.getDocument(parser.getFile().getPath());
+		Assert.assertTrue( parser.getCantCazas() == 4);
+		Assert.assertTrue( parser.getCantExploradores() == 6);
+		Assert.assertTrue( parser.getCantAvionetas() == 4);
+		Assert.assertTrue( parser.getCantBombarderos() == 3);
+		
+		parser.pasarNivel();
+		Assert.assertTrue( parser.getCantCazas() == 5);
+		Assert.assertTrue( parser.getCantExploradores() == 6);
+		Assert.assertTrue( parser.getCantAvionetas() == 5);
+		Assert.assertTrue( parser.getCantBombarderos() == 4);
+		
+		parser.pasarNivel();
+		Assert.assertTrue( parser.getCantCazas() == 6);
+		Assert.assertTrue( parser.getCantExploradores() == 7);
+		Assert.assertTrue( parser.getCantAvionetas() == 7);
+		Assert.assertTrue( parser.getCantBombarderos() == 5);
+		
 	}
 }
