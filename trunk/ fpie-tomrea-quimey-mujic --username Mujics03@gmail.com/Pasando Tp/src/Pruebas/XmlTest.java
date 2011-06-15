61c6b682420531152694b3b6e6ac9455a84a925e
fpiechotka@gmail.com <fpiechotka@gmail.com@fc4ff0fb-fbaf-c834-63c7-d19879779173>
2011-06-15 03:55:56 +0000

    Xml

--- trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/XmlTest.java
+++ trunk/ fpie-tomrea-quimey-mujic --username Mujics03@gmail.com/Pasando Tp/src/Pruebas/XmlTest.java
@@ -18,18 +18,24 @@ public class XmlTest
 		Assert.assertTrue( parser.getCantExploradores() == 6);
 		Assert.assertTrue( parser.getCantAvionetas() == 4);
 		Assert.assertTrue( parser.getCantBombarderos() == 3);
+		Assert.assertTrue( parser.getCantHelicopteros() == 1);
+		Assert.assertTrue( parser.getCantAvionCivil() == 1);
 		
 		parser.pasarNivel();
 		Assert.assertTrue( parser.getCantCazas() == 5);
 		Assert.assertTrue( parser.getCantExploradores() == 6);
 		Assert.assertTrue( parser.getCantAvionetas() == 5);
 		Assert.assertTrue( parser.getCantBombarderos() == 4);
+		Assert.assertTrue( parser.getCantHelicopteros() == 2);
+		Assert.assertTrue( parser.getCantAvionCivil() == 1);
 		
 		parser.pasarNivel();
 		Assert.assertTrue( parser.getCantCazas() == 6);
 		Assert.assertTrue( parser.getCantExploradores() == 7);
 		Assert.assertTrue( parser.getCantAvionetas() == 7);
 		Assert.assertTrue( parser.getCantBombarderos() == 5);
+		Assert.assertTrue( parser.getCantHelicopteros() == 2);
+		Assert.assertTrue( parser.getCantAvionCivil() == 2);
 		
 	}
 }
